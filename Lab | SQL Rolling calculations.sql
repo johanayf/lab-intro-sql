d4f7012aec6e95da6ea2444a58acd563fe06d6f7
Johana Yuan Feng <johanayf@Johanas-MacBook-Pro.local>
2022-02-13 17:14:59 +0100

    rolling calculator

--- Lab | SQL Rolling calculations.sql
+++ Lab | SQL Rolling calculations.sql
@@ -1,13 +1,33 @@
+use sakila;
+#Create View
+create view monthly_customer as
+select date_format(rental_date, '%M') as month, 
+date_format(rental_date, '%Y') as year, count(distinct customer_id) as active_customer
+from rental
+group by date_format(rental_date, '%M') , 
+date_format(rental_date, '%Y')
+order by year;
+
 ##Get number of monthly active customers.
+select * from monthly_customer;
 
 ##Active users in the previous month.
+select *,
+lag(active_customer, 1) over() as lastmonth_customer
+from monthly_customer
+;
 
 ##Percentage change in the number of active customers.
+select month, year, ((active_customer - lag(active_customer, 1) over())/active_customer*100) as lastmonth_percentage
+from monthly_customer;
+select * from monthly_customer;
 
 ##Retained customers every month.
-
-
-use salika;
-SELECT r.rental_id, r.rental_date, r.inventory_id, r.rental_date, f.film_id, f.title from rental as r
-left join inventory as i on i.inventory_id = r.inventory_id 
-left join film as f on f.film_id = i.film_id;
+with monthly_customer as (select distinct customer_id as active_customer, date_format(rental_date, '%M') as month,
+date_format(rental_date, '%Y') as year
+from rental)
+select m.month, m.year, count(distinct m.active_customer), (lag(m.month, 1) over()) as second_month from monthly_customer as m
+cross join monthly_customer as m2 
+where m.active_customer = m2.active_customer 
+group by m.month, m.year
+order by m.year;
